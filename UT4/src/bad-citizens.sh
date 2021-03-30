f36cc20cdbc5cc1e27d76f7dbfa490e8fe911267
lantolin <luis.antolin@gmail.com>
2021-03-30 20:56:40 +0200

    preparando la P.LINADM administracion de procesos

--- UT4/src/bad-citizens.sh
+++ UT4/src/bad-citizens.sh
@@ -2,9 +2,9 @@
 
 BASE=/root/1ASIR-ISO-code/UT4/src/
 
-nohup "${BASE}"/stress-ng --cpu-load 60 --cpu 1 &
-nohup "${BASE}"/munch2 400 &
+"${BASE}"/stress-ng --cpu-load 60 --cpu 1 &
+"${BASE}"/munch2 400 &
 for (( i=1; i<=20; i++ ))
 do 
- nohup "${BASE}"/zombieInfinity &
+  "${BASE}"/zombieInfinity &
 done
