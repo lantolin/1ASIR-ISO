835eddaad35aba11f763870b2c9452c82983cd00
lantolin <luis.antolin@gmail.com>
2021-03-30 21:06:51 +0200

    preparando la P.LINADM administracion de procesos

--- UT4/src/bad-citizens.sh
+++ UT4/src/bad-citizens.sh
@@ -2,9 +2,12 @@
 
 BASE=/root/1ASIR-ISO-code/UT4/src/
 
+if ps -fe | grep zombieInfinity | grep -v grep; then
+  exit 1
+fi
+
 "${BASE}"/stress-ng --cpu-load 60 --cpu 1 &
 "${BASE}"/munch2 400 &
-for (( i=1; i<=20; i++ ))
-do 
+for ((i = 1; i <= 20; i++)); do
   "${BASE}"/zombieInfinity &
 done
