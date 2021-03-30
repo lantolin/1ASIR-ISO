67192578c66c8ec423f785589ea88a43c1ddb2b0
lantolin <luis.antolin@gmail.com>
2021-03-30 20:43:35 +0200

    preparando la P.LINADM administracion de procesos

--- UT4/src/bad-citizens.sh
+++ UT4/src/bad-citizens.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./stress-ng --cpu-load 60 --cpu 1 &
-./munch2 400 &
-#for (( i=1; i<=20; i++ ))
-#do 
-#  ./zombieInfinity &
-#done
+nohup ./stress-ng --cpu-load 60 --cpu 1 &
+nohup ./munch2 400 &
+for (( i=1; i<=20; i++ ))
+do 
+ nohup  ./zombieInfinity &
+done
