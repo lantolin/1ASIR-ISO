5790dfa19487d8502a56cd00f65824fb0a9391ef
lantolin <luis.antolin@gmail.com>
2021-04-20 11:35:03 +0200

    stress-ng se instala por apt

--- UT4/src/bad-citizens.sh
+++ UT4/src/bad-citizens.sh
@@ -2,12 +2,15 @@
 
 BASE=/root/1ASIR-ISO-code/UT4/src/
 
+stress-ng --cpu-load 60 --cpu 1 &
+
+"${BASE}"/munch2 400 &
+
 if ps -fe | grep zombieInfinity | grep -v grep; then
   exit 1
-fi
+else
 
-"${BASE}"/stress-ng --cpu-load 60 --cpu 1 &
-"${BASE}"/munch2 400 &
-for ((i = 1; i <= 20; i++)); do
-  "${BASE}"/zombieInfinity &
-done
+  for ((i = 1; i <= 20; i++)); do
+    "${BASE}"/zombieInfinity &
+  done
+fi
