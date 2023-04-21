44ee6c7d6ca533ae3cbc8f0eeced90a2605c1ff1
Luis Antolin <luis.antolin@gmail.com>
2023-04-21 15:30:47 +0200

    mejoras bad-citizens P.LINADM-3

--- UT4/src/bad-citizens.sh
+++ UT4/src/bad-citizens.sh
@@ -2,17 +2,37 @@
 
 ### TODO: hay que mejorar esto para que solo cree los problemas si no existen ya
 
-
 BASE=/root/1ASIR-ISO/UT4/src/
+LOG=/tmp/bad-citizens.log
 
-stress-ng --cpu-load 60 --cpu 1 &
+echo "INICIO $(date +"%Y-%m-%d_%H.%M.%S")" >>${LOG}
 
-"${BASE}"/munch2 400 &
-
-if ! ps -fe | grep soyUnProceso | grep -v grep; then
-  for ((i = 1; i <= 50; i++)); do
-    "${BASE}"/soyUnProceso &
-  done
+if ps -fe | grep stress-ng | grep -v grep; then
+  echo "stress-ng ya esta corriendo" >>${LOG}
+else
+  echo "stress-ng se lanza" >>${LOG}
+  stress-ng --cpu-load 70 --cpu 1 &
 fi
 
+if ps -fe | grep munch | grep -v grep; then
+  echo "munch ya esta corriendo" >>${LOG}
+else
+  echo "munch se lanza" >>${LOG}
+  "${BASE}"/munch2 430 &
+fi
 
+NUM_ZOMBIS=$(ps -e -O status | grep -w Z | grep -v grep | wc -l)
+echo "NUM_ZOMBIS=${NUM_ZOMBIS}" >>${LOG}
+
+if [[ $NUM_ZOMBIS -gt 50 ]]; then
+  echo "suficientes zombis"  >>${LOG}
+else
+  echo "añadiendo zombis" >>${LOG}
+  if ! ps -fe | grep soyUnProceso | grep -v grep; then
+    for ((i = 1; i <= 50; i++)); do
+      "${BASE}"/soyUnProceso &
+    done
+  fi
+fi
+
+echo "FIN $(date +"%Y-%m-%d_%H.%M.%S")" >>${LOG}
